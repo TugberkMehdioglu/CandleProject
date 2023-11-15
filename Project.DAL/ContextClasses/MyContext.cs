54a3c5ef22533ec089c35b51594ff5134b7c71cd
Tuğberk <tugberkmehdioglu@gmail.com>
2023-11-15 10:37:24 +0300

    roles and userRoles dataseeds added

--- Project.DAL/ContextClasses/MyContext.cs
+++ Project.DAL/ContextClasses/MyContext.cs
@@ -20,6 +20,10 @@ namespace Project.DAL.ContextClasses
             base.OnModelCreating(builder);//For IdentityUserRole's primary key issue
 
             builder.ApplyConfigurationsFromAssembly(Assembly.Load("Project.MAP"));
+
+            //DataSeed for appusers's identity
+            SeedRoles(builder);
+            SeedUserRoles(builder);
         }
 
         public DbSet<Address>? Addresses { get; set; }
@@ -30,5 +34,38 @@ namespace Project.DAL.ContextClasses
         public DbSet<OrderDetail>? OrderDetails { get; set; }
         public DbSet<Product>? Products { get; set; }
 
+        private void SeedRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole>().HasData(
+            new IdentityRole()
+            {
+                Id = "4d7b3bc1-f3aa-48ce-b587-5e7dc5557634",
+                Name = "Admin",
+                NormalizedName = "ADMIN",
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            },
+            new IdentityRole()
+            {
+                Id = "4d7b3bc1-f3aa-48ce-b587-5e7dc5553134",
+                Name = "Member",
+                NormalizedName = "MEMBER",
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            });
+        }
+
+        private void SeedUserRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserRole<string>>().HasData(
+            new IdentityUserRole<string>()
+            {
+                UserId = "5c8defd5-91f2-4256-9f16-e7fa7546dec4",
+                RoleId = "4d7b3bc1-f3aa-48ce-b587-5e7dc5557634"
+            },
+            new IdentityUserRole<string>()
+            {
+                UserId = "5c8defd5-91f2-4256-9f16-e7fa7546fec5",
+                RoleId = "4d7b3bc1-f3aa-48ce-b587-5e7dc5553134"
+            });
+        }
     }
 }
