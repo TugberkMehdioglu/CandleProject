50a5f0b45c864334165f72a6dd9e664f0243de0a
Tuğberk <tugberkmehdioglu@gmail.com>
2023-11-15 15:51:35 +0300

    layer completed

---
+++ Project.DAL/Repositories/Concretes/AppUserRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Project.DAL.ContextClasses;
+using Project.DAL.Repositories.Abstracts;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Repositories.Concretes
+{
+    public class AppUserRepository : BaseRepository<AppUser>, IAppUserRepository
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        public AppUserRepository(MyContext context, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager) : base(context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public override async Task<IEnumerable<IdentityError>?> AddAsync(AppUser entity)
+        {
+            IdentityResult result = await _userManager.CreateAsync(entity);
+            if (result.Succeeded!) return result.Errors;
+
+            await _signInManager.SignInAsync(entity, true);
+            return null;
+        }
+    }
+}
