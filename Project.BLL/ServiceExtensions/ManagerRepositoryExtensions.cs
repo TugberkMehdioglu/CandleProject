cf66014b8afe6f9747f28311176b5cadb677b2f5
Tuğberk <tugberkmehdioglu@gmail.com>
2023-12-01 13:28:38 +0300

    Repositories and managers extension added

---
+++ Project.BLL/ServiceExtensions/ManagerRepositoryExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Project.BLL.ManagerServices.Abstarcts;
+using Project.BLL.ManagerServices.Concretes;
+using Project.DAL.Repositories.Abstracts;
+using Project.DAL.Repositories.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.ServiceExtensions
+{
+    public static class ManagerRepositoryExtensions
+    {
+        public static IServiceCollection AddManagerRepositoy(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IManager<>), typeof(BaseManager<>));
+            services.AddScoped<IAddressManager, AddressManager>();
+            services.AddScoped<IAppUserManager, AppUserManager>();
+            services.AddScoped<IAppUserProfileManager, AppUserProfileManager>();
+            services.AddScoped<ICategoryManager, CategoryManager>();
+            services.AddScoped<IOrderDetailManager, OrderDetailManager>();
+            services.AddScoped<IOrderManager, OrderManager>();
+            services.AddScoped<IProductManager, ProductManager>();
+
+            services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<IAddressRepository, AddressRepository>();
+            services.AddScoped<IAppUserRepository, AppUserRepository>();
+            services.AddScoped<IAppUserProfileRepository, AppUserProfileRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
