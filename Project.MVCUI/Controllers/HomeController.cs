36c496b587a17ed762b6f458fd22cbba24b1f594
Tuğberk <tugberkmehdioglu@gmail.com>
2023-12-10 12:14:54 +0300

    some changes for homePage

--- Project.MVCUI/Controllers/HomeController.cs
+++ Project.MVCUI/Controllers/HomeController.cs
@@ -7,12 +7,6 @@ namespace Project.MVCUI.Controllers
     [Route("[Controller]/[Action]")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         [Route("/")]
         [Route("/Home")]
