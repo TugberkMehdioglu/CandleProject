e7869a2541fa53e691348aef54f5b8c86bc36151
Tuğberk <tugberkmehdioglu@gmail.com>
2023-12-14 14:38:43 +0300

    Signin page created but not completed

---
+++ Project.MVCUI/ViewModels/AppUserProfileViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.MVCUI.ViewModels
+{
+    public class AppUserProfileViewModel
+    {
+        public string? Id { get; set; }
+
+        [Display(Name = "İsim")]
+        [Required(ErrorMessage ="{0} zorunludur")]
+        [StringLength(80, MinimumLength = 2, ErrorMessage = "{0}, {2} ile {1} karakter arasında olmalıdır")]
+        public string FirstName { get; set; } = null!;
+
+        [Display(Name ="Soyisim")]
+        [Required(ErrorMessage = "{0} zorunludur")]
+        [StringLength(80, MinimumLength = 2, ErrorMessage = "{0}, {2} ile {1} karakter arasında olmalıdır")]
+        public string LastName { get; set; } = null!;
+
+        [ValidateNever]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+
+        [ValidateNever]
+        public string? ImagePath { get; set; }
+
+        [ValidateNever]
+        public IFormFile? Image { get; set; }
+
+        //Navigation Properties
+
+    }
+}
---
+++ Project.MVCUI/ViewModels/WrapperClasses/SignupViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Project.MVCUI.ViewModels.WrapperClasses
+{
+    public class SignupViewModel
+    {
+        public AppUserViewModel AppUser { get; set; } = null!;
+        public AppUserProfileViewModel AppUserProfile { get; set; } = null!;
+    }
+}
