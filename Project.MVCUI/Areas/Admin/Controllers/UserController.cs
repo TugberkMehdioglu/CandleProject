7d2878ab332feeb99b2021c045c3746c97c39fb0
Tuğberk <tugberkmehdioglu@gmail.com>
2023-12-24 13:16:56 +0300

    UserList page completed

---
+++ Project.MVCUI/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.ENTITIES.Enums;
+using Project.ENTITIES.Models;
+using Project.MVCUI.Areas.Admin.AdminViewModels;
+
+namespace Project.MVCUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    [Route("[Area]/[Controller]/[Action]")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> UserList()
+        {
+            List<UserViewModel> userViewModels = await _userManager.Users.Where(x => x.Status != DataStatus.Deleted).Select(x => new UserViewModel()
+            {
+                Id = x.Id,
+                UserName = x.UserName,
+                Email = x.Email,
+                PhoneNumber = x.PhoneNumber,
+                EmailConfirmed = x.EmailConfirmed
+            }).ToListAsync();
+
+            foreach (UserViewModel user in userViewModels)
+            {
+                user.Roles = await _userManager.GetRolesAsync(new AppUser() { Id = user.Id });
+            }
+
+            return View(userViewModels);
+        }
+    }
+}
