10f6e60293e40255cc251ead20036ea45b2f6c38
Tuğberk <tugberkmehdioglu@gmail.com>
2023-12-26 13:28:31 +0300

    Product proccess starting

---
+++ Project.MVCUI/Areas/Admin/AdminViewModels/ProductViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project.MVCUI.Areas.Admin.AdminViewModels
+{
+    public class ProductViewModel
+    {
+        public int? Id { get; set; }
+
+        [Display(Name = "Ürün Adı")]
+        [Required(ErrorMessage = "{0} zorunludur")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "{0}, {2} ile {1} karakter arasında olmalıdır")]
+        public string Name { get; set; } = null!;
+
+        [Display(Name = "Açıklama")]
+        [Required(ErrorMessage = "{0} zorunludur")]
+        public string Description { get; set; } = null!;
+
+        [Display(Name = "Fiyat")]
+        [Required(ErrorMessage = "{0} zorunludur")]
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "{0} alanında noktadan sonra en fazla 2 basamak olmalıdır")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Stok")]
+        [Required(ErrorMessage = "{0} zorunludur")]
+        [Range(1, 100000, ErrorMessage = "{0} alanı {1} ile {2} arasında olmalıdır")]
+        [DataType(DataType.Text)]
+        public short Stock { get; set; }
+
+        [Display(Name = "Ürün Resmi")]
+        [Required(ErrorMessage = "{0} zorunludur")]
+        public string ImagePath { get; set; } = null!;
+
+        [Display(Name = "Ürün Kategorisi")]
+        [Required(ErrorMessage = "{0} zorunludur")]
+        public int CategoryID { get; set; }
+
+        public string? CategoryName { get; set; }
+    }
+}
---
+++ Project.MVCUI/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.BLL.ManagerServices.Abstarcts;
+using Project.ENTITIES.Models;
+using Project.MVCUI.Areas.Admin.AdminViewModels;
+
+namespace Project.MVCUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    [Route("[Area]/[Controller]/[Action]")]
+    public class ProductController : Controller
+    {
+        private readonly IProductManager _productManager;
+        private readonly IMapper _mapper;
+
+        public ProductController(IProductManager productManager, IMapper mapper)
+        {
+            _productManager = productManager;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet("{search?}/{pageNumber?}/{pageSize?}")]
+        public async Task<IActionResult> ProductList(string? search, int pageNumber = 1, int pageSize = 5)
+        {
+            IQueryable<Product> query = _productManager.GetActives();
+
+            if (search != null)
+                query.Where(x => x.Name.ToLower().Trim() == search.ToLower().Trim());
+
+
+            List<ProductViewModel> productViewModels = await query.OrderBy(x => x.CreatedDate)
+                .Skip((pageNumber - 1) * pageSize).Take(pageSize)
+                .Include(x => x.Category).Select(x => new ProductViewModel()
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Description = x.Description,
+                    Price = x.Price,
+                    Stock = x.Stock,
+                    ImagePath = x.ImagePath,
+                    CategoryID = x.Category.Id,
+                    CategoryName = x.Category.Name
+                }).ToListAsync();
+
+
+            int totalItemsCount = await query.CountAsync();
+
+
+            ViewBag.totalPageCount = (int)Math.Ceiling((double)totalItemsCount / pageSize);
+            ViewBag.pageNumber = pageNumber;
+            ViewBag.totalItemsCount = totalItemsCount;
+
+            return View(productViewModels);
+        }
+    }
+}
