6bd4f6afbfcefc0e935da0880ea04f2157bf1151
Tuğberk <tugberkmehdioglu@gmail.com>
2024-01-04 18:15:24 +0300

    cart components added

---
+++ Project.MVCUI/ShoppingTools/Cart.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+
+namespace Project.MVCUI.ShoppingTools
+{
+    [Serializable]
+    public class Cart
+    {
+        [JsonProperty]
+        private readonly Dictionary<int, CartItem> _basket;
+
+        public Cart()
+        {
+            _basket = new Dictionary<int, CartItem>();
+        }
+
+        [JsonProperty]
+        public ICollection<CartItem> Basket 
+        {
+            get
+            {
+                return _basket.Values;
+            } 
+        }
+
+        [JsonProperty]
+        public decimal TotalPrice 
+        {
+            get
+            {
+                return _basket.Sum(x => x.Value.SubTotal);
+            }
+        }
+
+        [JsonProperty]
+        public int TotalItemCount => _basket.Sum(x => x.Value.Amount);
+
+        public void AddToBasket(CartItem item)
+        {
+            if (_basket.ContainsKey(item.Id))
+            {
+                if (item.Amount > 1) _basket[item.Id].Amount += item.Amount;
+                else _basket[item.Id].Amount += 1;
+            }
+            else _basket.Add(item.Id, item);
+        }
+
+        public void RemoveFromBasket(int id)
+        {
+            if (_basket[id].Amount > 1) _basket[id].Amount -= 1;
+            else _basket.Remove(id);
+        }
+
+        public void RemoveItemWithAllAmountFromBasket(int id) => _basket.Remove(id);
+    }
+}
---
+++ Project.MVCUI/ShoppingTools/CartItem.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+
+namespace Project.MVCUI.ShoppingTools
+{
+    [Serializable]
+    public class CartItem
+    {
+        [JsonProperty]
+        public int Id { get; set; }
+
+        [JsonProperty]
+        public string Name { get; set; } = null!;
+
+        [JsonProperty]
+        public string Description { get; set; } = null!;
+
+        [JsonProperty]
+        public decimal Price { get; set; }
+
+        [JsonProperty]
+        public short Amount { get; set; }
+
+        [JsonProperty]
+        public string ImagePath { get; set; } = null!;
+
+        [JsonProperty]
+        public decimal SubTotal 
+        { 
+            get
+            {
+                return Price * Amount;
+            }
+        }
+
+        //For starts with 1
+        public CartItem()
+        {
+            ++Amount;
+        }
+    }
+}
