3f663f4b9de3e4658e2426a104577e5b334c5326
Tuğberk <tugberkmehdioglu@gmail.com>
2024-01-25 13:14:19 +0300

    order controller created

---
+++ Project.MVCUI/Areas/Member/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Project.BLL.ManagerServices.Abstarcts;
+
+namespace Project.MVCUI.Areas.Member.Controllers
+{
+    [Area("Member")]
+    [Authorize(Roles = "Member, Admin")]
+    [Route("[Area]/[Controller]/[Action]")]
+    public class OrderController : Controller
+    {
+        private readonly IOrderManager _orderManager;
+        private readonly IMapper _mapper;
+
+        public OrderController(IOrderManager orderManager, IMapper mapper)
+        {
+            _orderManager = orderManager;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
